cbcd9435400bdd7b84cfa51282b3c88ff44793be
kento <akiyama09141203@gmail.com>
2023-02-15 22:57:16 +0900

    update

--- pointnet2/pointnet2_utils.py
+++ pointnet2/pointnet2_utils.py
@@ -7,16 +7,22 @@ def fathest_point_sampling(xyz, n_points):
     xyz: tensor [batch size, num points, num dimension]
     n_points: number of sampling points
     """
-    B, N, D = xyz.shape
-    centroid_ids = tf.random.uniform([B, 1], minval=0, maxval=N-1, dtype=tf.int64)
+    def _gen_idx(b):
+        return [b, added_id[b]]
+
+    xyz_shape = tf.shape(xyz)
+    B = xyz_shape[0]
+    N = xyz_shape[1]
+    D = xyz_shape[2]
+    centroid_ids = tf.random.uniform([B, 1], minval=0, maxval=N-1, dtype=tf.int32)
     mask = tf.ones([B, N], dtype=tf.float32)
     for i in range(n_points-1):
         added_id = centroid_ids[:, i]
-        added_id_tf = [[b, added_id[b]] for b in range(B)]
+        added_id_tf = tf.stack([tf.range(B), added_id], axis=1) 
         added_point = tf.reshape(tf.gather_nd(xyz, added_id_tf), [B, 1, D])
         dist = tf.math.reduce_euclidean_norm(xyz - added_point, axis=2)
         dist = dist * mask
-        max_d_idx = tf.argmax(dist, axis=1)[..., tf.newaxis]
+        max_d_idx = tf.argmax(dist, axis=1, output_type=tf.int32)[..., tf.newaxis]
 
         centroid_ids = tf.concat([centroid_ids, max_d_idx], axis=1)
         mask = tf.math.minimum(dist * mask * 10e+10, mask)
@@ -31,15 +37,14 @@ def get_point_from_idx(xyz, idx):
     return:
         [B, n_points, D] or [B, n_points, n_samples, D]
     """
-
-
-    if len(idx.shape) == 2:
-        B, n = idx.shape
-        b_idx = tf.tile(tf.range(B, dtype=tf.int64)[..., tf.newaxis], [1, n])[..., tf.newaxis] # [B, n, 1]
+    idx_shape = tf.shape(idx)
+    if idx_shape.shape == 2:
+        B, n = idx_shape[0], idx_shape[1]
+        b_idx = tf.tile(tf.range(B, dtype=tf.int32)[..., tf.newaxis], [1, n])[..., tf.newaxis] # [B, n, 1]
         idx = idx[..., tf.newaxis]  # [B, n, 1]
         idx = tf.concat([b_idx, idx], axis=2)
-    elif len(idx.shape) == 3:
-        B, np, ns = idx.shape
+    elif idx_shape.shape == 3:
+        B, np, ns = idx_shape[0], idx_shape[1], idx_shape[2]
         b_idx = tf.tile(tf.reshape(tf.range(B, dtype=tf.int32), [B, 1, 1]), [1, np, ns])[..., tf.newaxis] # [B, np, ns, 1]
         idx = idx[..., tf.newaxis]  # [B, n, 1]
         idx = tf.concat([b_idx, idx], axis=3)
@@ -56,8 +61,15 @@ def query_ball_point(radius, n_samples, xyz, cent_xyz):
     return:
         [B, n_points, n_samples]
     """
-    B, N, D = xyz.shape
-    _, n, _ = cent_xyz.shape
+    xyz_shape = tf.shape(xyz)
+    B = xyz_shape[0]
+    N = xyz_shape[1]
+    D = xyz_shape[2]
+
+    n = tf.shape(cent_xyz)[1]
+
+    # B, N, D = xyz.shape
+    # _, n, _ = cent_xyz.shape
     xyz = tf.reshape(xyz, [B, 1, N, D])
     cent_xyz = tf.reshape(cent_xyz, [B, n, 1, D])
     dist = tf.math.reduce_euclidean_norm(tf.tile(xyz, [1, n, 1, 1]) - cent_xyz, axis=3)
@@ -97,7 +109,12 @@ def sample_and_group_all(xyz: tf.Tensor, points: tf.Tensor=None):
     return:
         [[B, n_samples, 3], [B, n_points, n_samples, D]]
     """
-    B, N, C = xyz.shape
+    xyz_shape = tf.shape(xyz)
+    B = xyz_shape[0]
+    N = xyz_shape[1]
+    C = xyz_shape[2]
+
+    # B, N, C = xyz.shape
 
     new_xyz = tf.zeros([B, 1, C])
     grouped_xyz = tf.reshape(xyz, [B, 1, N, C])
